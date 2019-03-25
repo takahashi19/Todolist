f9b08c01e15a27c0cae9c5cc4cb6e5a97be6064c
高橋 <icasenyou2018@gmail>
2019-03-25 15:56:10 +0900

    あげる前

--- todo.js
+++ todo.js
@@ -1,10 +1,7 @@
-alert("こんにちは！");
-alert(111 + 222);
-
 /*
 //【サブ課題１】まずはinputタグに入力された文字列を
 // タスクとして追加する処理を作成してみましょう!!
-*/ 
+*/
 
 //１．<iuput>タグを取得する
 //ヒント：document.getElementByID()という関数を調べてみましょう
@@ -13,7 +10,7 @@ alert(111 + 222);
 
 //３．<tr>タグと<td>タグを新たに作成する
 //ヒント：document.createElement()という関数を調べてみましょう
-var trElement = document.createElement('tr');
+var trElement = document.createElement("tr");
 
 //４．取得したinputタグの文字列を３で生成した<td>タグのテキストに代入する
 //ヒント：inputタグのvalue属性に、入力した文字列が格納されています
